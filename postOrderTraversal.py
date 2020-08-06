5978664d888f20085e0f6286b7f48ff556b0877a
11anubhav <50802334+11anubhav@users.noreply.github.com>
2020-08-07 03:32:18 +0530

    Add files via upload

---
+++ postOrderTraversal.py
@@ -0,0 +1,46 @@
+# python3 is used as a programming language
+#Without Recurrsion Solution for postorder traversal of a BST
+class newNode: 
+
+	# Constructor to create a newNode 
+    # visited is used as a flag
+	def __init__(self, data): 
+		self.data = data 
+		self.left = None
+		self.right = None
+		self.visited = False
+
+def postorder(head) : 
+
+	temp = head 
+	while (temp and temp.visited == False): 
+
+		# Visited left subtree 
+		if (temp.left and
+			temp.left.visited == False): 
+			temp = temp.left 
+
+		# Visited right subtree 
+		elif (temp.right and
+			temp.right.visited == False): 
+			temp = temp.right 
+
+		# Print node 
+		else: 
+			print(temp.data, end = " ") 
+			temp.visited = True
+			temp = head 
+						
+
+if __name__ == '__main__': 
+
+	root = newNode(8) 
+	root.left = newNode(3) 
+	root.right = newNode(10) 
+	root.left.left = newNode(1) 
+	root.left.right = newNode(6) 
+	root.left.right.left = newNode(4) 
+	root.left.right.right = newNode(7) 
+	root.right.right = newNode(14) 
+	root.right.right.left = newNode(13) 
+	postorder(root) 
